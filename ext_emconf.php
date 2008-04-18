e48d8bf7b39f77c32ac771922307f834675f899b
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-04-18 14:24:20 +0000

    version 0.0.2 from TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/paymentlibsaferpay/trunk@8947 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,24 +2,23 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "paymentlib_saferpay"
-# 
-# Auto generated 04-03-2006 20:22
-# 
+#
+# Auto generated 18-04-2008 16:16
+#
 # Manual updates:
-# Only the data in the array - anything else is removed by next write
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = Array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'Payment method "Saferpay"',
 	'description' => 'This extension contains the payment method for Saferpay',
 	'category' => 'misc',
 	'shy' => 0,
-	'dependencies' => 'cms,paymentlib',
+	'dependencies' => 'paymentlib,paysuite,static_info_tables',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '',
-	'PHP_version' => '',
 	'module' => '',
 	'state' => 'alpha',
 	'internal' => '',
@@ -29,12 +28,24 @@ $EM_CONF[$_EXTKEY] = Array (
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author' => 'Franz Holzinger',
-	'author_email' => 'kontakt@fholzinger.com',
+	'author_email' => 'contact@fholzinger.com',
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '0.0.1',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:12:{s:40:"class.tx_paymentlibipayment_provider.php";s:4:"4c3d";s:21:"ext_conf_template.txt";s:4:"e3f4";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"440b";s:18:"hidden_trigger.php";s:4:"af70";s:13:"locallang.php";s:4:"01e5";s:18:"paymentmethods.xml";s:4:"b80c";s:25:"res/paymentmethod_elv.gif";s:4:"01df";s:32:"res/paymentmethod_mastercard.gif";s:4:"44df";s:26:"res/paymentmethod_visa.gif";s:4:"d5ad";s:49:"tests/tx_paymentlibipayment_provider_testcase.php";s:4:"777d";s:57:"tests/fixtures/tx_paymentlibtranscentral_provider_fixture.inc";s:4:"620c";}',
+	'version' => '0.0.2',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.1.2-0.0.0',
+			'paymentlib' => '0.0.0-0.2.0',
+			'paysuite' => '0.0.1-',
+			'static_info_tables' => '2.0.5-',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"63d5";s:40:"class.tx_paymentlibsaferpay_provider.php";s:4:"94ef";s:21:"ext_conf_template.txt";s:4:"812b";s:12:"ext_icon.gif";s:4:"b82e";s:17:"ext_localconf.php";s:4:"b556";s:18:"hidden_trigger.php";s:4:"96a5";s:13:"locallang.php";s:4:"f06b";s:18:"paymentmethods.xml";s:4:"d48b";s:14:"doc/manual.sxw";s:4:"3ead";s:31:"res/logo_saferpay_color_low.png";s:4:"c258";s:46:"tests/tx_paymentsaferpay_provider_testcase.php";s:4:"42d3";s:57:"tests/fixtures/tx_paymentlibtcentral_provider_fixture.inc";s:4:"620c";}',
 );
 
 ?>
